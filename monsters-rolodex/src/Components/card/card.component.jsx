0448783cb78f846ff36c6acefd8f735de9a465c7
neo8848 <neosudarshan5@gmail.com>
2020-01-05 12:21:44 +1100

    card component debug

--- monsters-rolodex/src/Components/card/card.component.jsx
+++ monsters-rolodex/src/Components/card/card.component.jsx
@@ -4,7 +4,10 @@ import './card.styles.css';
 
 export const Card = props => (
     <div className='card-container'>
-    <img alt="monster" src="https://robohash.org/${props.monster.id}?set=set2&size=180x180"></img>
+    <img 
+    alt="monster"
+    src={`https://robohash.org/${props.monster.id}?set=set3&size=180x180`}
+    />
     <h1>
    {props.monster.name}
     </h1>
