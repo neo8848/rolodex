a79933bae3f518f3128b62e00dccc2dea8eecb22
neo8848 <neosudarshan5@gmail.com>
2020-01-05 11:27:19 +1100

    Updates

---
+++ precache-manifest.4dc364f32cdacab031f42d3b878fa4d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cebe745c2b9592e730f96c03deace6",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "6e33ade668ddbf70d2f3",
+    "url": "/rolodex/static/css/main.bde7072f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e33ade668ddbf70d2f3",
+    "url": "/rolodex/static/js/main.8cd7391d.chunk.js"
+  },
+  {
+    "revision": "d72009913ef41833d3a3",
+    "url": "/rolodex/static/js/runtime-main.2fc8d5fe.js"
+  }
+]);
